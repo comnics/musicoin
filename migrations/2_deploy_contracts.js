ee06bb71114de24b4aafa7463e01b185c6cc1e18
comnic <comnic@EXTPC055.EXT.PAYGATE.NET>
2018-01-16 11:44:52 +0900

    add migrations

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,11 @@
+
+const MusicoinCrowdsale = artifacts.require("./MusicoinCrowdsale.sol")
+
+module.exports = function(deployer, network, accounts) {
+  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
+  const endTime = startTime + (86400 * 20) // 20 days
+  const rate = new web3.BigNumber(1000)
+  const wallet = accounts[0]
+
+  deployer.deploy(MusicoinCrowdsale, startTime, endTime, rate, wallet)
+};
